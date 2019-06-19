bd219a7c3a061fc7becb113df95cd84bcc0e8883
Steve Forse <steve.forse@gmail.com>
2019-06-19 01:38:57 +0000

    Using expsoe loader to expose jQuery as global $ function

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,13 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
-
 }))
+
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [
+    { loader: 'expose-loader', options: '$' },
+  ]
+})
 
 module.exports = environment
