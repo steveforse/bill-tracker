fb6e727f43e3e7cde39153408c84035500066e45
Steve Forse <steve.forse@gmail.com>
2019-06-29 02:45:39 +0000

    Add dependent: :destroy to has_many relations to automatically delete schedules on payee deletion

--- app/models/payee.rb
+++ app/models/payee.rb
@@ -2,7 +2,7 @@ class Payee < ApplicationRecord
   strip_attributes
 
   # Relations
-  has_many :schedules
+  has_many :schedules, dependent: :destroy
 
   # Validations
   validates :name, presence: true, uniqueness: true, allow_blank: false, allow_nil: false
