f5be2680b0050b08bb8004b5eae2679f5a756c83
Steve Forse <steve.forse@gmail.com>
2019-06-29 02:43:16 +0000

    Removing index and show routes for schedules. Index moving to payee#show and show unnecessary for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :payees do
-    resources :schedules, shallow: true
+    resources :schedules, shallow: true, except: [:show, :index]
   end
 
   root 'payees#index'
