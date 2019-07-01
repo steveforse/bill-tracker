6b6326f2e0835edbd95ffd8529084a887b80e933
Steve Forse <steve.forse@gmail.com>
2019-07-01 19:34:16 +0000

    Implements schedule decorator specs

--- spec/decorators/schedule_decorator_spec.rb
+++ spec/decorators/schedule_decorator_spec.rb
@@ -3,4 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe ScheduleDecorator do
+  let(:schedule) do
+    build(:schedule).decorate
+  end
+
+  describe '#autopay' do
+    it 'renders autopay false as Disabled' do
+      schedule.autopay = false
+      expect(schedule.autopay).to eq('Disabled')
+    end
+
+    it 'renders autopay true as Enabled' do
+      schedule.autopay = true
+      expect(schedule.autopay).to eq('Enabled')
+    end
+  end
+
+  describe '#minimum_payment' do
+    it 'renders minimum payment as currency' do
+      schedule.minimum_payment = 42.3
+      expect(schedule.minimum_payment).to eq('$42.30')
+    end
+  end
+
+  describe '#frequency' do
+    it 'renders frequency as long description from model' do
+      schedule.frequency = 'weekly'
+      expect(schedule.frequency).to eq(Schedule.frequencies['weekly'])
+    end
+  end
 end
