2b65cdaef5aa55f96205c73b4237b41ef7084686
Steve Forse <steve.forse@gmail.com>
2019-07-01 19:32:42 +0000

    Rubocop reminded me to use Time.zone instead of Date

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :schedule do
-    start_date { Faker::Date.between(1.year.ago, Date.today).change(day: rand(1..28)) }
+    start_date { Faker::Date.between(1.year.ago, Time.zone.today).change(day: rand(1..28)) }
     end_date { Faker::Date.between(Date.tomorrow, 1.year.from_now) }
     frequency { Schedule.frequencies.keys.sample }
     autopay { Faker::Boolean.boolean }
