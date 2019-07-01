95b67d39ac57a7b7266a6cf1758e2f0de5b15776
Steve Forse <steve.forse@gmail.com>
2019-07-01 19:33:57 +0000

    Implements decorator specs

--- spec/decorators/payee_decorator_spec.rb
+++ spec/decorators/payee_decorator_spec.rb
@@ -3,4 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe PayeeDecorator do
+  let(:payee) do
+    build(:payee).decorate
+  end
+
+  describe '#website' do
+    it 'renders website as a link' do
+      url = 'https://www.google.com'
+      payee.website = url
+      expect(payee.website).to have_link(href: url, exact_text: url)
+    end
+  end
+
+  describe '#phone_number' do
+    it 'renders phone number as a link' do
+      phone_number = '555-555-5555'
+      payee.phone_number = phone_number
+      expect(payee.phone_number).to have_link(href: ('tel:' + phone_number),
+                                              exact_text: phone_number)
+    end
+  end
 end
