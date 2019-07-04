f30a5ad6c5c96b3bc96f1308549582b23236409d
Steve Forse <steve.forse@gmail.com>
2019-07-04 03:25:38 +0000

    Adding view tests for schedules

--- spec/views/schedules/edit.html.slim_spec.rb
+++ spec/views/schedules/edit.html.slim_spec.rb
@@ -3,9 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'schedules/edit', type: :view do
-  let(:schedule) do
-    create(:schedule)
-  end
+  let(:schedule) { create(:schedule) }
 
   it 'renders the edit schedule form' do
     assign(:schedule, schedule)
@@ -13,6 +11,35 @@ RSpec.describe 'schedules/edit', type: :view do
     render
 
     assert_select 'form[action=?][method=?]', schedule_path(schedule), 'post' do
+      assert_select 'div.form-inputs', count: 1 do
+        assert_select 'input', count: 5
+
+        assert_select 'label', text: 'Start date *'
+        assert_select 'input[data-toggle=datetimepicker][name="schedule[start_date]"]'
+
+        assert_select 'label', text: 'End date'
+        assert_select 'input[data-toggle=datetimepicker][name="schedule[end_date]"]'
+
+        assert_select 'label', text: 'Frequency'
+        assert_select 'select[name="schedule[frequency]"]' do
+          assert_select 'optgroup', count: 3
+          assert_select 'option', count: 9
+        end
+
+        assert_select 'label', text: 'Minimum payment'
+        assert_select 'input[type=number][name="schedule[minimum_payment]"]'
+
+        assert_select 'label', text: 'Frequency'
+        assert_select 'input[type=hidden][name="schedule[autopay]"]'
+        assert_select 'input[data-toggle=toggle][name="schedule[autopay]"]'
+      end
+
+      assert_select 'div.form-actions', count: 1 do
+        assert_select 'input.btn.btn-primary[type=submit][value="Update Schedule"]', count: 1
+        assert_select 'button.btn.btn-outline-secondary[type=reset]', text: 'Reset Form', count: 1
+        assert_select "a.btn.btn-outline-secondary[href='/payees/#{schedule.payee.id}']",
+                      text: 'Back to Payee', count: 1
+      end
     end
   end
 end
--- spec/views/schedules/new.html.slim_spec.rb
+++ spec/views/schedules/new.html.slim_spec.rb
@@ -12,6 +12,35 @@ RSpec.describe 'schedules/new', type: :view do
     render
 
     assert_select 'form[action=?][method=?]', payee_schedules_path(schedule.payee), 'post' do
+      assert_select 'div.form-inputs', count: 1 do
+        assert_select 'input', count: 5
+
+        assert_select 'label', text: 'Start date *'
+        assert_select 'input[data-toggle=datetimepicker][name="schedule[start_date]"]'
+
+        assert_select 'label', text: 'End date'
+        assert_select 'input[data-toggle=datetimepicker][name="schedule[end_date]"]'
+
+        assert_select 'label', text: 'Frequency'
+        assert_select 'select[name="schedule[frequency]"]' do
+          assert_select 'optgroup', count: 3
+          assert_select 'option', count: 9
+        end
+
+        assert_select 'label', text: 'Minimum payment'
+        assert_select 'input[type=number][name="schedule[minimum_payment]"]'
+
+        assert_select 'label', text: 'Frequency'
+        assert_select 'input[type=hidden][name="schedule[autopay]"]'
+        assert_select 'input[data-toggle=toggle][name="schedule[autopay]"]'
+      end
+
+      assert_select 'div.form-actions', count: 1 do
+        assert_select 'input.btn.btn-primary[type=submit][value="Create Schedule"]', count: 1
+        assert_select 'button.btn.btn-outline-secondary[type=reset]', text: 'Reset Form', count: 1
+        assert_select "a.btn.btn-outline-secondary[href='/payees/#{schedule.payee.id}']",
+                      text: 'Back to Payee', count: 1
+      end
     end
   end
 end
