a49a847f3ac5f9ab6ef577c6934feb2dde82135f
Steve Forse <steve.forse@gmail.com>
2019-07-11 01:46:04 +0000

    Adding routes for new endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
     resources :schedules, shallow: true, except: %i[show index]
   end
 
+  get 'calendar', to: 'calendar#index'
+  get 'calendar/events', to: 'calendar#events'
+
   root 'payees#index'
 end
