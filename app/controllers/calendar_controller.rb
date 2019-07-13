06dbd16ab9d310c8cd992e121ff14b4477d185e9
Steve Forse <steve.forse@gmail.com>
2019-07-13 23:25:20 +0000

    Make use of date range params from fullcalendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,5 +5,10 @@ class CalendarController < ApplicationController
   end
 
   def events
+    start_date = params[:start] || nil
+    end_date = params[:end] || nil
+
+    @payments = Payment.includes(:schedule).where(date: start_date..end_date)
+    @schedules = Schedule.includes(:payee).all
   end
 end
