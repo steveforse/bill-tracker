d3521b09bddbfa95a239f2577a7a4b36b7545d0d
Steve Forse <steve.forse@gmail.com>
2019-07-23 05:46:33 +0000

    payments request spec stub

--- spec/requests/payments_spec.rb
+++ spec/requests/payments_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Payments', type: :request do
-  describe 'GET /payments' do
+  describe 'GET /schedules/:schedule_id/payments' do
+    let(:schedule) { create(:schedule) }
     it 'works! (now write some real specs)' do
-      get payments_path
+      get schedule_payments_path(schedule)
       expect(response).to have_http_status(:ok)
     end
   end
