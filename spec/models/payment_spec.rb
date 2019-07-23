38caa127f97497dca495a41f5a9a91a81dcba184
Steve Forse <steve.forse@gmail.com>
2019-07-23 05:46:10 +0000

    Basic payment model spec

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:schedule) }
+  end
+
+  describe 'validations' do
+    subject(:model) { described_class.new }
+
+    # Schedule ID
+    it { is_expected.to validate_presence_of(:schedule_id) }
+
+    # Date
+    it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_date_of(:date) }
+
+    # Due Date
+    it { is_expected.to validate_presence_of(:due_date) }
+    it { is_expected.to validate_date_of(:due_date) }
+
+    # Amount
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
+  end
 end
