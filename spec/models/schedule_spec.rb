26562d3a783a1835f09512bf35fe7e7ec6c8544b
Steve Forse <steve.forse@gmail.com>
2019-07-26 06:04:51 +0000

    Adding rrule_string tests

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -29,9 +29,64 @@ RSpec.describe Schedule, type: :model do
         .in_array(Schedule.frequencies.keys)
         .with_message('must be from dropdown list')
     end
+  end
 
-    # Association exists
-    it { is_expected.to allow_value(create(:payee).id).for(:payee_id) }
-    it { is_expected.not_to allow_value(9999).for(:payee_id) }
+  describe 'self.frequencies' do
+    %w[weekly biweekly quadweekly monthly bimonthly semimonthly trimonthly
+       annually semiannually].each do |frequency|
+      it { expect(described_class.frequencies).to include(frequency) }
+      it "has correct attributes for #{frequency} frequency" do
+        expect(described_class.frequencies[frequency]).to include(:description)
+          .and include(:frequency)
+          .and include(:interval)
+      end
+    end
+  end
+
+  describe '#rrule_string' do
+    let(:schedule) do
+      Schedule.new(
+        name: 'example',
+        start_date: Time.zone.today,
+        end_date: Time.zone.tomorrow
+      )
+    end
+
+    %w[weekly biweekly quadweekly monthly bimonthly trimonthly annually].each do |frequency|
+      it "generates correct rrule for #{frequency} frequency" do
+        schedule.frequency = frequency
+        expect(schedule.rrule_string)
+          .to include("DTSTART=#{schedule.start_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("DTEND=#{schedule.end_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("FREQ=#{Schedule.frequencies[schedule.frequency][:frequency].upcase}")
+          .and include("INTERVAL=#{Schedule.frequencies[schedule.frequency][:interval]}")
+      end
+
+      it 'skips end_date if not set' do
+        schedule.frequency = frequency
+        schedule.end_date = nil
+        expect(schedule.rrule_string).not_to include("DTEND=")
+      end
+    end
+
+    it 'generates correct rrule for semimonthly' do
+      schedule.frequency = 'semimonthly'
+      expect(schedule.rrule_string)
+          .to include("DTSTART=#{schedule.start_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("DTEND=#{schedule.end_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("FREQ=#{Schedule.frequencies[schedule.frequency][:frequency].upcase}")
+          .and include("INTERVAL=#{Schedule.frequencies[schedule.frequency][:interval]}")
+          .and include("BYMONTHDAY=#{schedule.start_date.day},")
+    end
+
+    it 'generates correct rrule for semiannually' do
+      schedule.frequency = 'semiannually'
+      expect(schedule.rrule_string)
+          .to include("DTSTART=#{schedule.start_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("DTEND=#{schedule.end_date.strftime('%Y%m%dT%H%M%S')}")
+          .and include("FREQ=#{Schedule.frequencies[schedule.frequency][:frequency].upcase}")
+          .and include("INTERVAL=#{Schedule.frequencies[schedule.frequency][:interval]}")
+          .and include("BYMONTH=#{schedule.start_date.month},")
+    end
   end
 end
