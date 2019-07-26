90d95c13c64610265d0d3defa3b55e18be8e5000
Steve Forse <steve.forse@gmail.com>
2019-07-26 04:54:20 +0000

    Adding test for payment decorator. Only checks that amount is converted to currency string

--- spec/decorators/payment_decorator_spec.rb
+++ spec/decorators/payment_decorator_spec.rb
@@ -3,6 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe PaymentDecorator do
-  it 'does something' do
+  let(:payment) do
+    build(:payment).decorate
+  end
+
+  describe '#amount' do
+    it 'renders amount as currency' do
+      payment.amount= 42.3
+      expect(payment.amount).to eq('$42.30')
+    end
   end
 end
