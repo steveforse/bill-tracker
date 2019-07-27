aa53008720d7f9eed4f10332ea6adc96977c6833
Steve Forse <steve.forse@gmail.com>
2019-07-27 04:40:16 +0000

    Fix logic for due_date

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -16,10 +16,13 @@ class Payment < ApplicationRecord
   # Custom validator
   validate :due_date_between_schedule_start_and_end_dates
 
+  # rubocop: disable Metrics/AbcSize
   def due_date_between_schedule_start_and_end_dates
-    if (due_date.present? && due_date < schedule.start_date) ||
-       (schedule.end_date.present? && due_date > schedule.end_date)
-      errors.add(:due_date, 'must be between schedule start and end dates')
-    end
+    return if schedule.blank? || due_date.blank? ||
+              (due_date >= schedule.start_date && schedule.end_date.nil?) ||
+              due_date.between?(schedule.start_date, schedule.end_date)
+
+    errors.add(:due_date, 'must be between schedule start and end dates')
   end
+  # rubocop: enable Metrics/AbcSize
 end
