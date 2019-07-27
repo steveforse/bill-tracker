49e764bc0eac2fd8da55dff95436a1ce5be14a49
Steve Forse <steve.forse@gmail.com>
2019-07-27 01:19:06 +0000

    Ensure that payment date is between schedule start and end date

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -12,4 +12,14 @@ class Payment < ApplicationRecord
   validates :date, date: true, presence: true
   validates :due_date, date: true, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
+
+  # Custom validator
+  validate :due_date_between_schedule_start_and_end_dates
+
+  def due_date_between_schedule_start_and_end_dates
+    if (due_date.present? && due_date < schedule.start_date) ||
+       (schedule.end_date.present? && due_date > schedule.end_date)
+      errors.add(:due_date, 'must be between schedule start and end dates')
+    end
+  end
 end
