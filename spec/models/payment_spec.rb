f7bdf90e539fdef5cde821821174d60f04173bd5
Steve Forse <steve.forse@gmail.com>
2019-07-27 06:19:35 +0000

    Tests for custom validation methods on payments model

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe Payment, type: :model do
     it { is_expected.to validate_presence_of(:amount) }
     it { is_expected.to validate_numericality_of(:amount).is_greater_than(0) }
   end
+
+  describe 'custom validators' do
+    describe 'due_date_between_schedule_start_and_end_dates' do
+      let(:payment) { create(:payment) }
+
+      it 'cannot have due_date before start_date' do
+        payment.due_date = payment.schedule.start_date - 1.month
+        payment.valid?
+        expect(payment.errors.messages[:due_date]).to \
+          include('must be after schedule start date')
+      end
+
+      it 'cannot have due_date after end_date' do
+        payment.due_date = payment.schedule.end_date + 1.month
+        payment.valid?
+        expect(payment.errors.messages[:due_date]).to \
+          include('must be before schedule end date')
+      end
+    end
+  end
 end
